21ae30d07be7a4a0cb216d06b7cfe62433ac50e0
cjvictor12 <itsjustvictor10@gmail.com>
2025-04-06 18:06:42 +0100

    Add files via upload

---
+++ lib/login.dart
@@ -0,0 +1,110 @@
+import 'package:flutter/material.dart';
+import 'package:uni_pulse_combined/organized/user_home_page.dart';
+import 'package:uni_pulse_combined/organized/org_main.dart';
+
+
+class AuthScreen extends StatefulWidget {
+  const AuthScreen({super.key});
+
+  @override
+  State<AuthScreen> createState() {return _AuthScreenState();}
+}
+
+class _AuthScreenState extends State<AuthScreen> {
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+  final TextEditingController confirmPasswordController =
+      TextEditingController();
+
+  void showErrorDialog(String message) {
+    showDialog(
+      context: context,
+      builder: (ctx) => AlertDialog(
+        title: Text("Error"),
+        content: Text(message),
+        actions: [
+          TextButton(
+            onPressed: () => Navigator.of(ctx).pop(),
+            child: Text("OK"),
+          ),
+        ],
+      ),
+    );
+  }
+
+  void handleAuth() {
+    String email = emailController.text.trim();
+    String password = passwordController.text.trim();
+
+    if (email.isEmpty || password.isEmpty) {
+      showErrorDialog("All fields are required.");
+      return;
+    }
+
+    if (email == 'user' && password == '123') {
+      Navigator.pop(context);
+      Navigator.of(context)
+          .pushReplacement(MaterialPageRoute(builder: (ctx) => HomePage()));
+          return;
+    }
+    if (email == 'org' && password == '123') {
+      Navigator.pop(context);
+      Navigator.of(context)
+          .pushReplacement(MaterialPageRoute(builder: (ctx) => MainPage()));
+          return;
+    } else {
+      showErrorDialog("Invalid credentials. Please try again.");
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Row(
+        children: [
+          Expanded(
+            child: Padding(
+              padding: EdgeInsets.all(20.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    "Login",
+                    style: TextStyle(
+                        fontSize: 24,
+                        fontWeight: FontWeight.bold,
+                        color: const Color.fromARGB(255, 101, 3, 121)),
+                  ),
+                  SizedBox(height: 20),
+                  TextField(
+                    controller: emailController,
+                    decoration: InputDecoration(
+                        labelText: "Email", prefixIcon: Icon(Icons.email)),
+                  ),
+                  SizedBox(height: 10),
+                  TextField(
+                    controller: passwordController,
+                    obscureText: true,
+                    decoration: InputDecoration(
+                        labelText: "Password", prefixIcon: Icon(Icons.lock)),
+                  ),
+                  SizedBox(height: 20),
+                  ElevatedButton(
+                    onPressed: handleAuth,
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.teal.shade400,
+                      foregroundColor: Colors.white,
+                      padding:
+                          EdgeInsets.symmetric(horizontal: 40, vertical: 12),
+                    ),
+                    child: Text("LOGIN"),
+                  )
+                ],
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
