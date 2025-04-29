742a81b21efaad4b6a24305b6e970c7392dc8028
Sakiinah Adeyemi <up2248776@myport.ac.uk>
2025-04-29 13:37:34 +0100

    database to events and account

--- lib/Models/acconts.dart
+++ lib/Models/acconts.dart
@@ -1,16 +1,28 @@
 
-class AccountData {
-  const AccountData({
-    required this.firstName,
-    required this.lastName,
-    required this.phoneNum,
-    required this.email,
-    required this.pass,
-  });
+// class AccountData {
+//   const AccountData({
+//     required this.firstName,
+//     required this.lastName,
+//     required this.phoneNum,
+//     required this.email,
+//     required this.pass,
+//   });
 
-  final String firstName;
-  final String lastName;
-  final int phoneNum;
+//   final String firstName;
+//   final String lastName;
+//   final int phoneNum;
+//   final String email;
+//   final String pass;
+// }
+
+class AccountData {
   final String email;
-  final String pass;
+  final String password; // Added password field
+  final bool isOrganisation;
+
+  AccountData({
+    required this.email,
+    required this.password, // Added password to constructor
+    required this.isOrganisation,
+  });
 }
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,9 +5,11 @@
 import FlutterMacOS
 import Foundation
 
+import cloud_firestore
 import file_selector_macos
 import firebase_auth
 import firebase_core
+import firebase_storage
 import location
 import path_provider_foundation
 import pdf_render
@@ -15,9 +17,11 @@ import sqflite_darwin
 import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FLTFirebaseFirestorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseFirestorePlugin"))
   FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
   FLTFirebaseAuthPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAuthPlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
+  FLTFirebaseStoragePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseStoragePlugin"))
   LocationPlugin.register(with: registry.registrar(forPlugin: "LocationPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SwiftPdfRenderPlugin.register(with: registry.registrar(forPlugin: "SwiftPdfRenderPlugin"))
